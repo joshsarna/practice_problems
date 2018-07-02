510c1f4e2ffad6688fb9f9608cef6ed03521ec79
joshsarna <joshsarna@gmail.com>
2018-07-02 09:40:10 -0700

    organize whiteboarding questions into folders by date

---
+++ white_boarding_questions/7_1_18/q1.rb
@@ -0,0 +1,5 @@
+=begin
+
+Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is [“a”, “b”, “c”, “d”, “e”, “f”], the output should be [“a”, “c”, “e”].
+
+=end
