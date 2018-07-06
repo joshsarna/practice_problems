b9d7c8393dbc8d25204d5850a931118a4609bd87
joshsarna <joshsarna@gmail.com>
2018-07-05 22:11:51 -0700

    finish euler problem 18

--- project_euler/test.rb
+++ project_euler/test.rb
@@ -1,23 +1,23 @@
-start = Time.now
+# start = Time.now
 
-divisors =[]
-current_triangle = 0
-natural_number = 1
-while divisors.length <= 150 do
-  current_triangle += natural_number
-  natural_number += 1
-  divisors = []
-  i = 1
-  (current_triangle/2).times do
-    if current_triangle % i == 0
-      divisors << i
-    end
-    i += 1
-  end
-  divisors << current_triangle
-end
+# divisors =[]
+# current_triangle = 0
+# natural_number = 1
+# while divisors.length <= 150 do
+#   current_triangle += natural_number
+#   natural_number += 1
+#   divisors = []
+#   i = 1
+#   (current_triangle/2).times do
+#     if current_triangle % i == 0
+#       divisors << i
+#     end
+#     i += 1
+#   end
+#   divisors << current_triangle
+# end
 
-p current_triangle
+# p current_triangle
 
-finish = Time.now
-p finish - start
+# finish = Time.now
+# p finish - start
