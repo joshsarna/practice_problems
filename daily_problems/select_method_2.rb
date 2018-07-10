a3735892bbb00cb556ee216e27e14596faacce49
joshsarna <joshsarna@gmail.com>
2018-07-09 20:19:28 -0700

    finish select_method_2 daily problem

---
+++ daily_problems/select_method_2.rb
@@ -0,0 +1,24 @@
+=begin
+
+Use the `select` method with the following array of hashes
+
+movies = [
+  {id: 1, title: "Die Hard", rating: 5.0},
+  {id: 2, title: "Bad Boys", rating: 4.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+to create a new array of hashes that only contain movies with a rating less than 4.0.
+
+=end
+
+movies = [
+  {id: 1, title: "Die Hard", rating: 5.0},
+  {id: 2, title: "Bad Boys", rating: 4.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+bad_movies = movies.select{|movie| movie[:rating] < 4}
+p bad_movies  # => [{:id=>3, :title=>"The Chamber", :rating=>3.0}, {:id=>4, :title=>"Fracture", :rating=>2.0}]
