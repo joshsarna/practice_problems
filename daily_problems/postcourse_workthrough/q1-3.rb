b53509cf8b36391360741bd80c979b027f2d56dd
joshsarna <joshsarna@gmail.com>
2018-09-07 14:45:48 -0700

    finish daily problem 1-3

---
+++ daily_problems/postcourse_workthrough/q1-3.rb
@@ -0,0 +1,26 @@
+=begin
+
+Read about the Ruby `while` loop. Then refactor the code below using `while`. *Warning* - a `while` loop will run forever if you write it incorrectly (unlike a `times` loop or an `each` loop). You can enter Ctrl C in your terminal to force Ruby to quit if it’s stuck in an infinite loop.
+
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+numbers.length.times do
+  number = numbers[index]
+  sum = sum + number
+  index = index + 1
+end
+p sum
+
+
+=end
+
+numbers = [1, 2, 4, 2]
+sum = 0
+i = 0
+while i < numbers.length do
+  sum += numbers[i]
+  i += 1
+end
+
+p sum  # => 9
