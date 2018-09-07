74b73a64fb45f9003f48ba8e69eaee87bb3e50a5
joshsarna <joshsarna@gmail.com>
2018-09-07 14:48:20 -0700

    finish daily problem 1-4

---
+++ daily_problems/postcourse_workthrough/q1-4.rb
@@ -0,0 +1,24 @@
+=begin
+
+The following code will run 100 times, and each time it will ask the user for their name. The code will break early if the user’s name is Bob.
+
+100.times do
+  puts "What is your name?"  
+  name = gets.chomp
+  if name == "Bob"
+    break
+  end
+end
+
+puts "Hi, Bob!"
+
+Rewrite the code using a while loop so the program will run *forever* unless the user enters a name of Bob.
+
+=end
+
+name = ""
+while name != "Bob" do
+  puts "What is your name?"  
+  name = gets.chomp
+end
+puts "Hi, Bob!"
