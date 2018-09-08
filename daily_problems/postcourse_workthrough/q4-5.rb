44a102a0014b6258a5570d1bee187991241b0de5
joshsarna <joshsarna@gmail.com>
2018-09-07 22:34:16 -0700

    finish daily problem 5-5

---
+++ daily_problems/postcourse_workthrough/q4-5.rb
@@ -0,0 +1,15 @@
+=begin
+
+Write a method called `sum_of_range`, which will accept an array containing two numbers, and return the sum of all of the whole numbers within the range of those numbers, inclusive.
+
+p sum_of_range([1, 4])  #=> 10
+p sum_of_range([4, 1])  #=> 10
+
+=end
+
+def sum_of_range(range)
+  (range.min..range.max).sum
+end
+
+p sum_of_range([1, 4])  #=> 10
+p sum_of_range([4, 1])  #=> 10
