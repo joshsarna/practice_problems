b2589f837039c473c2d3acc2e7b3f1b1b14d105d
joshsarna <joshsarna@gmail.com>
2018-09-07 22:28:03 -0700

    finish daily problem 4-3

---
+++ daily_problems/postcourse_workthrough/q4-3.rb
@@ -0,0 +1,30 @@
+=begin
+
+Use the methods `map`, `select`, and `reduce` with the following array of hashes: 
+
+items = [
+  {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+  {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+  {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+  {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+  {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+]
+
+and calculate the total amount of words for all items of type `:book`. The result should be
+
+3906
+
+=end
+
+items = [
+  {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+  {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+  {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+  {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+  {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+]
+
+books = items.select{ |item| item[:type] == :book }
+word_lengths = books.map{ |book| book[:words] }
+total_length = word_lengths.reduce(:+)
+p total_length  # => 3906
