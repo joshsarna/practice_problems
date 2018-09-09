25cc19cc1326f388c73843148cd4cb2043d7fec7
joshsarna <joshsarna@gmail.com>
2018-09-08 22:27:07 -0700

    finish daily problem 6-1

---
+++ daily_problems/postcourse_workthrough/q6-1.rb
@@ -0,0 +1,17 @@
+=begin
+
+Read about the Ruby ternary operator. Then refactor the code below using a ternary operator.
+
+x = 74
+if x > 10
+  result = "big number"
+else
+  result = "small number"
+end
+puts result
+
+=end
+
+x = 74
+number_size = x > 10 ? 'big number' : 'small number'
+p number_size
