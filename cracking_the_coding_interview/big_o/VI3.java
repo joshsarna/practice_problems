4335dfb6ff61b9ae4f7eb8515c7e3cfbc4ff01b5
joshsarna <joshsarna@gmail.com>
2019-03-28 00:13:00 -0700

    finish big o question 3 in ctci

---
+++ cracking_the_coding_interview/big_o/VI3.java
@@ -0,0 +1,15 @@
+public class VI3 {
+  public static int mod(int a, int b) {
+    if (b <= 0) {
+      return -1;
+    }
+    int div = a / b;
+    return a - div * b;
+  }
+  public static void main(String[] args) {
+    int x = mod(4, 3);
+    System.out.println(x);
+  }
+}
+
+// O (1)
