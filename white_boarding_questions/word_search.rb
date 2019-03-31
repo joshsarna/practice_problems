8875d2931fb533af2e1383807239071e426b24a3
joshsarna <joshsarna@gmail.com>
2019-03-30 21:28:15 -0700

    finish initial solution for word search

---
+++ white_boarding_questions/word_search.rb
@@ -0,0 +1,41 @@
+def rotate_matrix(matrix)
+  n = matrix.length
+  m = matrix[0].length
+  new_matrix = Array.new(m, Array.new(n))
+  matrix.each_with_index do |row, row_i|
+    row.each_with_index do |col, col_i|
+      new_matrix[col_i][-row_i - 1] = col
+    end
+  end
+  return new_matrix
+end
+
+def word_search(input_matrix, search_term, recursion_counter = 0, count = 0)
+  # if recursion_counter == 4
+  return count if recursion_counter == 4
+  # end
+  matrix = input_matrix
+  # 4.times do
+  matrix.each_with_index do |row, row_i|
+    row.each.with_index do |col, col_i|
+      word_present = true
+      search_term.each_char.with_index do |letter, letter_i|
+        if matrix[row_i][col_i + letter_i] != search_term[letter_i]
+          word_present = false
+          break
+        end
+      end
+      if word_present == true
+        count += 1
+      end
+    end
+  end
+  word_search(rotate_matrix(matrix), search_term, recursion_counter + 1, count)
+    # matrix = rotate_matrix(matrix)
+  # end
+end
+
+p word_search([["n", "o"], ["n", "o"]], "no")  # => 2
+p word_search([["n", "o"], ["n", "o"]], "hi")  # => 0
+p word_search([["n", "o"], ["o", "n"]], "no")  # => 4
+# p word_search([['B', 'N', 'E', 'Y', 'S'], ['H', 'E', 'D', 'E', 'S'], ['S', 'G', 'N', 'D', 'E']], 'DES')
