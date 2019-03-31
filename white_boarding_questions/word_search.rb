f8f6292cb7c91630133df8c49bf26f42ffbfe188
joshsarna <joshsarna@gmail.com>
2019-03-30 21:59:32 -0700

    allow for bendy words in word_search

--- white_boarding_questions/word_search.rb
+++ white_boarding_questions/word_search.rb
@@ -42,7 +42,36 @@ def word_search(input_matrix, search_term, recursion_counter = 0, count = 0)
   # end
 end
 
-p word_search([["n", "o"], ["n", "o"]], "no")  # => 2
-p word_search([["n", "o"], ["n", "o"]], "hi")  # => 0
-p word_search([["n", "o"], ["o", "n"]], "no")  # => 4
-# p word_search([['B', 'N', 'E', 'Y', 'S'], ['H', 'E', 'D', 'E', 'S'], ['S', 'G', 'N', 'D', 'E']], 'DES')
+# p word_search([["n", "o"], ["n", "o"]], "no")  # => 2
+# p word_search([["n", "o"], ["n", "o"]], "hi")  # => 0
+# p word_search([["n", "o"], ["o", "n"]], "no")  # => 4
+
+def bendy_word_search(matrix, search_term)
+  finds = []
+  matrix.each.with_index do |row, row_i|
+    row.each.with_index do |col, col_i|
+      if col == search_term[0]
+        finds << [row_i, col_i]
+      end
+    end
+  end
+
+  search_term[1..-1].each_char do |letter|
+    new_finds = []
+    finds.each do |find|
+      test_next_letters = [[find[0] + 1, find[1]], [find[0] - 1, find[1]], [find[0], find[1] + 1], [find[0], find[1] - 1]].select{ | tnl | tnl.min >= 0 && tnl[0] < matrix.length && tnl[1] < matrix[0].length }
+      test_next_letters.each do |test_next_letter|
+        if matrix[test_next_letter[0]][test_next_letter[1]] == letter
+          new_finds << test_next_letter
+        end
+      end
+    end
+    finds = new_finds
+  end
+  finds.count
+end
+
+p bendy_word_search([
+  ['B', 'N', 'E', 'Y', 'S'],
+  ['H', 'E', 'D', 'E', 'S'],
+  ['S', 'G', 'N', 'D', 'E']], 'DES')
